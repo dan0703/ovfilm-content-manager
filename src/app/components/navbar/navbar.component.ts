9f31b93ae53ca21b798ea3be4ec6deba0772ed45
Dan Joshua <73008872+dan0703@users.noreply.github.com>
2025-02-24 22:21:10 -0600

    unused import deleted

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router, RouterLink, RouterOutlet } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
